af09afefbff815bc813a67cdf519b4460a9ca872
TshepisoDev <71729650+thefoenixweb@users.noreply.github.com>
2024-01-25 08:27:54 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# whatsapp bot to get live status update on corona virus
+
+from pymongo import MongoClient
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import datetime
+import urllib
+from corona1 import get_data
+
+
+# database connectivity
+cluster = MongoClient("mongodb+srv://john:Father@cluster0.b1lrdrw.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["covid"]
+users = db["users"]
+
+appbot = Flask(__name__)
+
+
+@appbot.route("/sms", methods=["get", "post"])
+def reply():
+    num = request.form.get("From")
+    num = num.replace("whatsapp:", "")
+    msg_text = request.form.get("Body")
+    x = users.find_one({"NUMBER": num})
+    try:
+        status = x["status"]
+    except:
+        pass
+
+    if (bool(x) == False):
+        users.insert_one({"NUMBER": num, "status": "first"})
+        msg = MessagingResponse()
+        resp = msg.message(
+            "Hello, this is a covid statistics update bot. Enter any country to receive covid infection rate statistics")
+        return (str(msg))
+    else:
+        if (status == "first"):
+            data = get_data(msg_text)
+            msg = MessagingResponse()
+            resp = msg.message(data)
+            users.delete_one({"NUMBER": num})
+            return (str(msg))
+
+
+# endregion
+if (__name__ == "__main__"):
+    appbot.run(port=5000)
