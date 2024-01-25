e8cb682b40dc0059347c6e7c7657bddc0c4410a4
TshepisoDev <71729650+thefoenixweb@users.noreply.github.com>
2024-01-25 14:23:18 +0200

    Update corona1.py

--- corona1.py
+++ corona1.py
@@ -1,40 +1,36 @@
 import requests
 import json
-import pandas as pd
-import matplotlib.pyplot as plt
-
-from reportlab.lib.styles import LineStyle
 
 
-def get_data(msg_text,num):
- url = "https://covid-193.p.rapidapi.com/statistics"
- headers = {
-    'x-rapidapi-host': "covid-193.p.rapidapi.com",
-    'x-rapidapi-key': "7d18fd6581msh81a4886d454a322p1e64c5jsn2c784c87b0cf"
+def get_data(msg_text, num):
+    url = "https://covid-193.p.rapidapi.com/statistics"
+
+    headers = {
+        "X-RapidAPI-Key": "b0e8adfb2fmshb9c958d198bb1d4p13a9f0jsn28997e5d2f03",
+        "X-RapidAPI-Host": "covid-193.p.rapidapi.com"
     }
 
- response = requests.request("GET", url, headers=headers)
- data=response.text
- data=json.loads(data)
- for i in range(len(data["response"])):
-    x=data["response"][i]["country"]
-    if(x.lower()==msg_text.lower()):
-        total=data["response"][i]["cases"]["total"]
-        active_cases=data["response"][i]["cases"]["active"]
-        recovered=data["response"][i]["cases"]["recovered"]
-        critical_cases=data["response"][i]["cases"]["critical"]
-        new_cases=data["response"][i]["cases"]["new"]
-        total_deaths=data["response"][i]["deaths"]["total"]
-        new_deaths=data["response"][i]["deaths"]["new"]
-        data_complete="Data of "+x+"\n"+"""total_infected: """+str(total)+"""
-active_case: """+str(active_cases)+"""
-recovered: """+str(recovered)+"""
-critical_cases: """+str(critical_cases)+"""    
-new_cases: """+str(new_cases)+"""
-total_death: """+str(total_deaths)+"""
-new_deaths: """+str(new_deaths)
+    response = requests.request("GET", url, headers=headers)
+    data = response.text
+    data = json.loads(data)
+    for i in range(len(data["response"])):
+        x = data["response"][i]["country"]
+        if (x.lower() == msg_text.lower()):
+            total = data["response"][i]["cases"]["total"]
+            active_cases = data["response"][i]["cases"]["active"]
+            recovered = data["response"][i]["cases"]["recovered"]
+            critical_cases = data["response"][i]["cases"]["critical"]
+            new_cases = data["response"][i]["cases"]["new"]
+            total_deaths = data["response"][i]["deaths"]["total"]
+            new_deaths = data["response"][i]["deaths"]["new"]
+            data_complete = "Data of " + x + "\n" + """total_infected: """ + str(total) + """
+active_case: """ + str(active_cases) + """
+recovered: """ + str(recovered) + """
+critical_cases: """ + str(critical_cases) + """    
+new_cases: """ + str(new_cases) + """
+total_death: """ + str(total_deaths) + """
+new_deaths: """ + str(new_deaths)
 
-        return (data_complete)
-        break
+            return (data_complete)
 
-    return ("country entered not found")
+
